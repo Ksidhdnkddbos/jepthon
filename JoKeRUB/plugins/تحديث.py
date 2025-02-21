51889aa01e1420ea2e78b51bf818955b66617f78
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-02-22 00:09:25 +0300

    Update تحديث.py

--- JoKeRUB/plugins/تحديث.py
+++ JoKeRUB/plugins/تحديث.py
@@ -326,7 +326,7 @@ async def Hussein(event):
             f"I guess you are on selfhost. For self host you need to use `{cmdhd}update now`",
         )
     event = await edit_or_reply(event, "**᯽︙ جارِ تحديث ريبو التنصيب لسورس الجوكر **")
-    off_repo = "https://github.com/jepthoniq/test"
+    off_repo = "https://github.com/Ksidhdnkddbos/jepthon"
     os.chdir("/app")
     try:
         txt = (
