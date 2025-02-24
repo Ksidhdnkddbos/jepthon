b883c8f60bf5c1611733ac850a84b72b56c8cdc2
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-02-24 14:48:46 +0300

    Update startup.py

--- JoKeRUB/utils/startup.py
+++ JoKeRUB/utils/startup.py
@@ -179,7 +179,7 @@ async def add_bot_to_logger_group(chat_id):
         except Exception as e:
             LOGS.error(str(e))
 #by @jepthon بس اشوفك خامطه للكود اهينك وافضحك
-JoKeRUB = {"@AljokerUserBot", "@jepthonsupport"}
+JoKeRUB = {"@aqhvv"}
 
 async def saves():
     for lMl10l in JoKeRUB:
