ab7a83b619969c4241e5e8d87956702e16d1e94e
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-02-24 18:52:10 +0300

    Update اغنية.py

--- JoKeRUB/plugins/اغنية.py
+++ JoKeRUB/plugins/اغنية.py
@@ -74,10 +74,59 @@ async def _(event):
 
     # البحث باستخدام الكوكيز
     video_link = await yt_search(str(query), cookies=cookies)
-    if not url(video_link):
+    if not video_link or video_link == "Couldnt fetch results":
         return await catevent.edit(
             f"⌔∮ عذرا لم استطع ايجاد مقاطع ذات صلة بـ `{query}`"
         )
+    
+    # تحميل الفيديو كملف صوتي
+    try:
+        cmd = event.pattern_match.group(1)
+        q = "320k" if cmd == "320" else "128k"
+        song_cmd = song_dl.format(QUALITY=q, video_link=video_link)
+        name_cmd = name_dl.format(video_link=video_link)
+        
+        stderr = (await _catutils.runcmd(song_cmd))[1]
+        if stderr:
+            return await catevent.edit(f"**خطأ :** `{stderr}`")
+        
+        catname, stderr = (await _catutils.runcmd(name_cmd))[:2]
+        if stderr:
+            return await catevent.edit(f"**خطأ :** `{stderr}`")
+        
+        catname = os.path.splitext(catname)[0]
+        song_file = Path(f"{catname}.mp3")
+        catname = urllib.parse.unquote(catname)
+        
+        if not os.path.exists(song_file):
+            return await catevent.edit(
+                f"⌔∮ عذرا لم استطع ايجاد مقاطع ذات صلة بـ `{query}`"
+            )
+        
+        await catevent.edit("**⌔∮ جارِ الارسال انتظر قليلاً**")
+        catthumb = Path(f"{catname}.jpg")
+        if not os.path.exists(catthumb):
+            catthumb = Path(f"{catname}.webp")
+        elif not os.path.exists(catthumb):
+            catthumb = None
+        
+        title = catname.replace("./temp/", "").replace("_", "|")
+        await event.client.send_file(
+            event.chat_id,
+            song_file,
+            force_document=False,
+            caption=f"**العنوان:** `{title}`",
+            thumb=catthumb,
+            supports_streaming=True,
+            reply_to=reply_to_id,
+        )
+        await catevent.delete()
+        for files in (catthumb, song_file):
+            if files and os.path.exists(files):
+                os.remove(files)
+    except ChatSendMediaForbiddenError as err:
+        await catevent.edit("لا يمكن ارسال المقطع الصوتي هنا")
+        LOGS.error(str(err))
 
 @l313l.ar_cmd(
     pattern="فيديو(?:\s|$)([\s\S]*)",
