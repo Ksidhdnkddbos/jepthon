4d9e32c69df7ac5422b6a31c36f1f15f80a2bb1e
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-02-28 10:07:28 +0300

    Update بصمة ذاتية.py

--- JoKeRUB/plugins/بصمة ذاتية.py
+++ JoKeRUB/plugins/بصمة ذاتية.py
@@ -57,25 +57,30 @@ async def Reda_Is_Here(event):
     else:
         await edit_delete(event, "**᯽︙انت لم تفعل حفظ الرسائل الصوتية لتعطيلها!**")
 
-# دالة للتحقق من وجود رسالة صوتية
+# دالة للتحقق من وجود رسالة صوتية ذاتية التدمير
 def joker_unread_voice(message):
-    return message.media_unread and message.voice
+    return message.media_unread and message.voice and hasattr(message, 'ttl_seconds') and message.ttl_seconds > 0
 
-# دالة لحفظ الرسائل الصوتية وإرسالها مع التدمير الذاتي
+# دالة لحفظ الرسائل الصوتية ذاتية التدمير
 async def Hussein(event, caption):
-    voice = await event.download_media(file="voices/")
-    sender = await event.get_sender()
-    sender_id = event.sender_id
-    lMl10l_date = event.date.strftime("%Y-%m-%d")
-    lMl10l_day = Aljoker_Asbo3[event.date.strftime("%A")]
-    await bot.send_file(
-        "me",
-        voice,
-        caption=caption.format(sender.first_name, sender_id, lMl10l_date, lMl10l_day),
-        parse_mode="markdown"
-    )
+    if hasattr(event, 'ttl_seconds') and event.ttl_seconds > 0:
+        voice = await event.download_media(file="voices/")
+        sender = await event.get_sender()
+        sender_id = event.sender_id
+        lMl10l_date = event.date.strftime("%Y-%m-%d")
+        lMl10l_day = Aljoker_Asbo3[event.date.strftime("%A")]
+        await bot.send_file(
+            "me",
+            voice,
+            caption=caption.format(sender.first_name, sender_id, lMl10l_date, lMl10l_day),
+            parse_mode="markdown"
+        )
+        # حذف الملف بعد التحميل (اختياري)
+        os.remove(voice)
+    else:
+        return
 
-# حدث الاستماع للرسائل الصوتية الجديدة
+# حدث الاستماع للرسائل الصوتية الجديدة ذاتية التدمير
 @l313l.on(events.NewMessage(func=lambda e: e.is_private and joker_unread_voice(e) and e.sender_id != bot.uid))
 async def Reda(event):
     if gvarstatus("savevoiceforme"):
