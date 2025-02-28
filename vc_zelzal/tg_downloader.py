b6c1804ff04120f0ddb23e482fe18431defdd64a
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-02-28 20:57:56 +0300

    Update tg_downloader.py

--- vc_zelzal/tg_downloader.py
+++ vc_zelzal/tg_downloader.py
@@ -7,9 +7,9 @@ from datetime import datetime
 
 from telethon.tl import types
 from telethon.utils import get_extension
-from ..Config import Config
-from ..core.managers import edit_or_reply
-from ..helpers import progress
+from JoKeRUB.Config import Config
+from JoKeRUB.core.managers import edit_or_reply
+from JoKeRUB.helpers import progress
 
 NAME = "untitled"
 
